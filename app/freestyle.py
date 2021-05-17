5452494a7348e397d3bd4f3f9c5de5caa19a066b
aredfern15 <77812013+aredfern15@users.noreply.github.com>
2021-05-17 00:25:02 -0400

    updated parsed_response

--- app/freestyle.py
+++ app/freestyle.py
@@ -1,39 +1,12 @@
 import json
 import os
+from typing import KeysView
 import requests
 from dotenv import load_dotenv 
 #import pandas as pd    #most likely use 
 #from datetime import datetime  #most likely use 
 
 load_dotenv()
-
-
-#INFO INPUTS
-#worked with Nolan Matsko and Annabelle Zebrowski 
-#checking if ticker has a valid number of characters
-
-
-api_key = os.environ.get("api_key") 
-request_url = f"https://fly.sportsdata.io/v3/mlb/scores/json/Standings/2021?key=5df11bf8ab924b3f8daa67c66e4e617c"
-response = requests.get(request_url)
-#request_url = f"https://fly.sportsdata.io/v3/mlb/scores/json/Standings/{season}?key={api_key}"
-
-#print(type(response))
-#print(response.status_code)
-#print(response.text)
-#
-#
-parsed_response = json.loads(response.text)
-
-quit()
-#set up API_KEY and possibly default settings
-
-league = os.getenv("league", default="AL")
-division = os.getenv("division", default="East")
-team = os.getenv("team", default="Baltimore Orioles")
-app_env = os.getenv("APP_ENV", default="development")
-
-
 
 
 season = input("Please enter the season you would like to view the MLB standings: ")
@@ -44,9 +17,39 @@ else:
     print("Oops, you have entered an invalid season. Please try again with a symbol such as '2019'.")
     exit()
 
-#check if season has a year
+#check if season has a season 
 
-#helped by olisteadman https://stackoverflow.com/questions/19859282/check-if-a-string-contains-a-number
+
+api_key = os.environ.get("api_key") 
+#request_url = f"https://fly.sportsdata.io/v3/mlb/scores/json/Standings/2021?key=5df11bf8ab924b3f8daa67c66e4e617c"
+request_url = f"https://fly.sportsdata.io/v3/mlb/scores/json/Standings/{season}?key={api_key}"
+response = requests.get(request_url)
+
+
+#print(type(response))
+#print(response.status_code)
+#print(response.text)
+#
+#
+parsed_response = json.loads(response.text)
+#print(parsed_response)
+
+
+quit()
+
+
+
+
+
+#set up API_KEY and possibly default settings
+
+league = os.getenv("league", default="AL")
+division = os.getenv("division", default="East")
+team = os.getenv("team", default="Baltimore Orioles")
+app_env = os.getenv("APP_ENV", default="development")
+
+
+
 
 #season_digits = False 
 
