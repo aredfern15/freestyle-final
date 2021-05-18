e324afe48ae12cdc3ae3933459cc01b2ec89ec1c
agz9 <77708021+agz9@users.noreply.github.com>
2021-05-18 11:47:01 -0400

    Capitalization and multi year

--- app/back_up.py
+++ app/back_up.py
@@ -4,38 +4,37 @@ from typing import KeysView
 import requests
 from dotenv import load_dotenv 
 #import pandas as pd    #most likely use 
-import datetime  #most likely use 
+import datetime 
 load_dotenv()
-
+api_key = os.environ.get("api_key") 
 
 season = input("Please enter the season you would like to view the MLB standings (2020 or 2021): ")
 if season == "2020" or "2021": 
      pass
 else: 
-     print("Oops, you have entered an invalid season. Please try again with a symbol such as '2020' or '2021'.")
+     print("Oops, you have entered an invalid season. Please try again with a year such as '2020' or '2021'.")
      exit()
 
-#check if season has a season 
-api_key = os.environ.get("api_key") 
-request_url = f"https://fly.sportsdata.io/v3/mlb/scores/json/Standings/2020?key={api_key}"
-response = requests.get(request_url)
-#print(type(response))
-#print(response.status_code)
-#print(response.text)
-parsed_response = json.loads(response.text)
-#print(parsed_response)
-
-list_of_team_names = []
-for line in parsed_response: 
-     list_of_team_names.append(line['Name'])
-print(list_of_team_names)
-
 team = input("Please enter the team you would like to view: ")
-capitalized_team = team.capitalize()
-index = list_of_team_names.index(capitalized_team)
-print(index)
+capitalized_team = team.title()
+list_of_team_names = []
 
-team_index = index 
+if season == '2020': 
+     request_url = f"https://fly.sportsdata.io/v3/mlb/scores/json/Standings/{season}?key={api_key}"
+     response = requests.get(request_url)
+     parsed_response = json.loads(response.text)
+     for line in parsed_response: 
+          list_of_team_names.append(line['Name'])
+     team_index = list_of_team_names.index(capitalized_team)
+     print(list_of_team_names)
+else:
+     request_url = f"https://fly.sportsdata.io/v3/mlb/scores/json/Standings/{season}?key={api_key}"
+     response = requests.get(request_url)
+     parsed_response = json.loads(response.text)
+     for line in parsed_response: 
+          list_of_team_names.append(line['Name'])
+     team_index = list_of_team_names.index(capitalized_team)
+
 city = parsed_response[team_index]["City"]
 league = parsed_response[team_index]["League"]
 name = parsed_response[team_index]["Name"]
@@ -46,34 +45,15 @@ division_rank = parsed_response[team_index]["DivisionRank"]
 games_behind = parsed_response[team_index]["GamesBehind"]
 
 
-#key = parsed_response[0]["Key"]
-
-
-#team = input("Please enter the team you would like to view: ")
-
-
-
-#if team == "Twins":
-#    TEAM = parsed_response[0]["Name"]
-#else: print("nope, try again")
-
-
-
-
-
 print("-------------------------")
-print(f"Season: {season}")
+print(f"SEASON: {season}")
 print(f"SELECTED TEAM: {city} {name}")
 print("-------------------------")
-#print("REQUESTING TEAM PERFORMANCE")
-#print("-------------------------")
 import time
 named_tuple = time.localtime() 
 time_string = time.strftime("%Y-%m-%d, %H:%M:%S", named_tuple)
 run_time_date = datetime.datetime.now()
 print("RUN AT: " + run_time_date.strftime("%I:%M %p") + " on " + run_time_date.strftime("%B %d") + ", " + run_time_date.strftime("%Y"))
-#print("-------------------------")
-#print(f"LATEST DAY: {LAST_REFRESHED}")
 print(f"LEAGUE: {league}")
 print(f"DIVISION: {division}")
 print(f"WINS: {wins}")
